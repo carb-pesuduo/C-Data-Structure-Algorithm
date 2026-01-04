c4405d414488cd11eba941aa152ba1da8a6b3cf4
WhiteDogHacker <growbel65@gmail.com>
2026-01-04 22:57:56 +0800

    modified:   experiment.cpp

--- experiment.cpp
+++ experiment.cpp
@@ -1,105 +1,27 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-class Stack {
-    private:
-        int top;
-        int arr[5];
-    public:
-        Stack() {
-            top = -1;
-            for (int i = 0; i < 5; i++) {
-                arr[i] = 0;
+int main() {
+
+    int arry[] = {43, 23, 75, 21, 20, 12, 72, 34};
+
+    int n = sizeof(arry) / sizeof(arry[0]);
+
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arry[j] > arry[j+1]) {
+                int temp = arry[j];
+                arry[j] = arry[j+1];
+                arry[j+1] = temp;
             }
         }
+    }
 
-        void push(int val) {
-            // Check if the array has space
-            if (top < 5 - 1) {
-                top++;
-                arr[top] = val;
-            }
-            else {
-                cout << "Stack Overflow" << endl;
-            }
-        }
-
-        void pop() {
-            // Check if the array has value/s
-            if (top > -1) {
-                int temp = arr[top];
-                arr[top] = 0;
-                top--;
-                cout << "Popped: " << temp << endl;
-            }
-            else {
-                cout << "Stack Underflow" << endl;
-            }
-        }
-
-        void peek() {
-             if (top > -1) {
-               cout << "Peeked at " << arr[top] << endl;
-            }
-            else {
-                cout << "Nothing to peek at" << endl;
-            }
-        }
-
-        void display() {
-            for (int i = 0; i < 5; i++) {
-                cout << arr[i] << endl;
-            }
-        }
-};
-
-
-int main()
-{
-    Stack a;
-    int userIntput;
-    do 
-    {
-        cout << "\nStack Data Structure | Enter 0 to exit" << endl
-             << "1.Enter an integer Value" << endl
-             << "2.Pop at the last value" << endl
-             << "3.Peek at the last value" << endl
-             << "4. Display all values" << endl;
-        
-        cout << "Enter Your Choice: ";
-        cin >> userIntput;
-        switch (userIntput)
-        {
-        case 0:
-            break;
-
-        case 1:
-            int val;
-            cout << "Enter a value to append: ";
-            cin >> val;
-            a.push(val);
-            break;
-
-        case 2:
-            a.pop();
-            break;
-            
-        case 3:
-            a.peek();
-            break;
-
-        case 4:
-            a.display();
-            break;
-
-        default:
-            cout << "Enter a valid input" << endl;
-            break;
-        }
-    
-    }while(userIntput != 0);
-    
+    cout << "Bubble Sort: ";
+    for (int num: arry) {
+        cout << num << " ";
+    }
     return 0;
+
 }
